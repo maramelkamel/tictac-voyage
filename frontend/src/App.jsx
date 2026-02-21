306b23121ef3524b9f0b0d348569bab73e85d217
maramelkamel <elkamelmaram6@gmail.com>
2026-02-21 19:07:53 +0100

    style pages sign in and create account

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from './pages/HomePage';
 import Omra from './pages/Omra/Omra';
 import Reserve from './pages/Omra/Reserve';
 import Details from './pages/Omra/Details';
+import CreateAccount from './pages/CreateAccount';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/Omra/Omra" element={<Omra />} />
         <Route path="/Omra/Details" element={<Details />} />
         <Route path="/Omra/Reserve" element={<Reserve />} />
+        <Route path="/CreateAccount" element={<CreateAccount />} />
+        <Route path="/SignIn" element={<SignIn />} />
       </Routes>
     </Router>
   );
